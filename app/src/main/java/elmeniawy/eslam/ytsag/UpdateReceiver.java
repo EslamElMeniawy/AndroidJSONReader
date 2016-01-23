bc4e44641e3b09810243bfd8761abe10fe33b0fc
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2016-01-23 18:23:40 +0200

    Add services and receivers for update

---
+++ app/src/main/java/elmeniawy/eslam/ytsag/UpdateReceiver.java
@@ -0,0 +1,47 @@
+package elmeniawy.eslam.ytsag;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by Eslam El-Meniawy on 23-Jan-16.
+ */
+public class UpdateReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(context, UpdateService.class);
+        startWakefulService(context, service);
+    }
+
+    public void setAlarm(Context context) {
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, UpdateReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                AlarmManager.INTERVAL_DAY,
+                AlarmManager.INTERVAL_DAY,
+                alarmIntent);
+        ComponentName receiver = new ComponentName(context, UpdateBootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    public void cancelAlarm(Context context) {
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, UpdateReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        alarmMgr.cancel(alarmIntent);
+        ComponentName receiver = new ComponentName(context, UpdateBootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+}
---
+++ app/src/main/java/elmeniawy/eslam/ytsag/UpdateService.java
@@ -0,0 +1,82 @@
+package elmeniawy.eslam.ytsag;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Eslam El-Meniawy on 23-Jan-16.
+ */
+public class UpdateService extends IntentService {
+    public static final int NOTIFICATION_ID = 2;
+
+    public UpdateService() {
+        super("YTSUpdateService");
+    }
+
+    @Override
+    protected void onHandleIntent(final Intent intent) {
+        VolleySingleton volleySingleton = VolleySingleton.getInstance();
+        final RequestQueue requestQueue = volleySingleton.getRequestQueue();
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, "https://yts.ag/api/v2/list_movies.json?limit=1&page=1", new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                if (response != null && response.length() > 0) {
+                    try {
+                        if (response.has("success") && response.getBoolean("success") && response.has("version") && !response.isNull("version") && response.has("url") && !response.isNull("url")) {
+                            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+                            int verCode = pInfo.versionCode;
+                            if (response.getInt("version") > verCode) {
+                                SharedPreferences sharedPreferences = MyApplication.getAppContext().getSharedPreferences("YTSPref", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putBoolean("updateAvailable", true);
+                                sendNotification();
+                                AlarmReceiver.completeWakefulIntent(intent);
+                            }
+                        }
+                    } catch (JSONException e) {
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        requestQueue.add(request);
+    }
+
+    private void sendNotification() {
+        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, SplashActivity.class), 0);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.update_notification_alert_title))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(getString(R.string.update_notification_alert_msg)))
+                .setContentText(getString(R.string.update_notification_alert_msg))
+                .setAutoCancel(true)
+                .setSound(alarmSound);
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+}
